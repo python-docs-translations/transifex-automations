e6174de8079bab2d8bebb9967f951d869b4ae843
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2025-08-01 01:51:22 -0300

    Add entry to remove-msgid.sh

--- scripts/remove-msgid.sh
+++ scripts/remove-msgid.sh
@@ -52,3 +52,7 @@ remove_msgid  reference/lexical_analysis.pot  '^\\N$'
 # Fixed via https://github.com/python/cpython/pull/19470
 remove_msgid  c-api/sys.pot '^Raises an :ref:`auditing event <auditing>` ``sys.addaudithook`` with no arguments.$'
 remove_msgid  library/sys.pot '^Raises an :ref:`auditing event <auditing>` ``sys.addaudithook`` with no arguments.$'
+
+# "Object" is too common Word, and should not be translated in this case.
+# Remove to avoid incorrect translation.
+remove_msgid library/string.templatelib.pot '^object$"
